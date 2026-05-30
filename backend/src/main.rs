07b2c9f79c2232d9264cb837d55132f38aaab679
Jeremy Cavallo <jcbball55@gmail.com>
2026-05-30 09:21:16 -0600

    Add clap, make simple name repeat CLI

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello, {}", args.name)
+    }
 }
