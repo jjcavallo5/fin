67a30cd97fc5ea92c2100ba7cb797de9bae9e4a9
Jeremy Cavallo <jcbball55@gmail.com>
2026-05-30 11:12:44 -0600

    Finally getting response from plaid

--- backend/src/link.rs
+++ backend/src/link.rs
@@ -1,4 +1,6 @@
 use crate::utils;
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::{env, process};
 
 fn load_env() -> (String, String) {
@@ -14,8 +16,47 @@ fn load_env() -> (String, String) {
     return (client_id, secret);
 }
 
+#[derive(Serialize, Deserialize)]
+struct User {
+    client_user_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct LinkRequest {
+    client_id: String,
+    secret: String,
+    client_name: String,
+    country_codes: Vec<String>,
+    language: String,
+    products: Vec<String>,
+    user: User,
+}
+
 pub fn link() {
     let (client_id, secret) = load_env();
 
-    println!("{}, {}", client_id, secret)
+    let request = LinkRequest {
+        client_id,
+        secret,
+        client_name: "FIN".to_string(),
+        country_codes: vec!["US".to_string()],
+        language: "en".to_string(),
+        products: vec!["auth".to_string()],
+        user: User {
+            client_user_id: "Jeremy".to_string(),
+        },
+    };
+
+    let client = reqwest::blocking::Client::new();
+
+    let resp = client
+        .post("https://sandbox.plaid.com/link/token/create")
+        .header("Content-Type", "application/json")
+        .json(&request)
+        .send()
+        .unwrap();
+
+    println!("resp = {resp:?}");
+    let body = resp.text().unwrap();
+    println!("body = {body:?}");
 }
