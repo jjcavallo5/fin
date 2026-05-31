ca4652fcfea7c4029bfc736d9577b0e6614718ea
Jeremy Cavallo <jcbball55@gmail.com>
2026-05-31 06:26:45 -0600

    Add balance return type

--- src/balance/types.rs
+++ src/balance/types.rs
@@ -1,9 +1,34 @@
 use serde::{Deserialize, Serialize};
-use tokio::sync::{oneshot, Mutex};
 
 #[derive(Serialize)]
 pub struct GetAccountRequest {
     pub client_id: String,
     pub secret: String,
     pub access_token: String,
+}
+
+#[derive(Deserialize)]
+struct Balance {
+    balance: f32,
+}
+
+#[derive(Deserialize)]
+struct Account {
+    account_id: String,
+    balances: Balance,
+    name: String,
+    official_name: String,
+    subtype: String,
+}
+
+#[derive(Deserialize)]
+struct Item {
+    institution_name: String,
+}
+
+#[derive(Deserialize)]
+pub struct GetAccountResponse {
+    accounts: Vec<Account>,
+    item: Item,
+    request_id: String,
 }
