a432e88b5e6ee40441842c2f66e410addd49eb81
Jeremy Cavallo <jcbball55@gmail.com>
2026-06-01 19:44:53 -0600

    Better DB file creation handling

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,23 @@
+use crate::logging;
 use sea_orm::{Database, DatabaseConnection};
 
-pub async fn get_db() -> DatabaseConnection {
+fn create_db() -> std::path::PathBuf {
     let db_path = dirs::home_dir().unwrap().join(".fin/plans.db");
-    println!("{}", db_path.display());
+    let exists = std::fs::exists(&db_path).unwrap_or_else(|_| {
+        logging::error("failed to create database");
+        std::process::exit(1)
+    });
+    if !exists {
+        std::fs::File::create_new(&db_path).unwrap_or_else(|_| {
+            logging::error("failed to create database");
+            std::process::exit(1)
+        });
+    }
+    return db_path;
+}
+
+pub async fn get_db() -> DatabaseConnection {
+    let db_path = create_db();
     let db = Database::connect(format!("sqlite://{}", db_path.display()))
         .await
         .unwrap();
